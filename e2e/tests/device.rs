e86f9184e878e546b13c35d4b29fb4317a13f010
alexsteffensen <asteff20@student.aau.dk>
2025-04-04 13:05:00 +0200

    added delete device test

--- e2e/tests/device.rs
+++ e2e/tests/device.rs
@@ -55,7 +55,7 @@ async fn can_link_device() {
 }
 
 #[tokio::test]
-async fn can_delete_device() {
+async fn can_unlink_device() {
     let address = "127.0.0.1:8071";
     let mut server = TestSamServer::start(address, None).await;
 
@@ -107,8 +107,57 @@ async fn can_delete_device() {
 }
 
 #[tokio::test]
-async fn can_delete_account() {
+async fn can_delete_device() {
     let address = "127.0.0.1:8072";
+    let mut server = TestSamServer::start(address, None).await;
+
+    server
+        .started_rx()
+        .await
+        .expect("Should be able to start server");
+
+    let username = "Alice";
+    let device_name = "Alice's Device";
+
+    let mut alice: DenimClient<SqliteDenimClientType> = DenimClient::from_registration()
+        .regular_store_config(SqliteSignalStoreConfig::in_memory().await)
+        .denim_store_config(SqliteSignalStoreConfig::in_memory().await)
+        .sam_store_config(SqliteSamStoreConfig::in_memory().await)
+        .api_client_config(HttpClientConfig::new(address.to_owned()))
+        .username(username)
+        .device_name(device_name)
+        .call()
+        .await
+        .expect("Can register account");
+
+    let token = alice
+        .create_provision()
+        .await
+        .expect("Can create a link token");
+
+    let id_key_pair = alice
+        .identity_key_pair()
+        .await
+        .expect("Can get id key pair");
+
+    let other_client: DenimClient<SqliteDenimClientType> = DenimClient::from_provisioning()
+        .api_client_config(HttpClientConfig::new(address.to_owned()))
+        .regular_store_config(SqliteSignalStoreConfig::in_memory().await)
+        .denim_store_config(SqliteSignalStoreConfig::in_memory().await)
+        .sam_store_config(SqliteSamStoreConfig::in_memory().await)
+        .device_name("Alice's Other Device")
+        .id_key_pair(id_key_pair)
+        .token(token)
+        .call()
+        .await
+        .expect("Can link device");
+
+    assert!(other_client.delete_device().await.is_ok())
+}
+
+#[tokio::test]
+async fn can_delete_account() {
+    let address = "127.0.0.1:8073";
     let mut server = TestSamServer::start(address, None).await;
 
     server
