8bcd7033de7e82d6d97dfa45895919f041bbd479
alexsteffensen <asteff20@student.aau.dk>
2025-04-28 10:45:28 +0200

    implemented tests for block_list and request_manager

--- proxy/src/managers/in_mem/block_list.rs
+++ proxy/src/managers/in_mem/block_list.rs
@@ -14,13 +14,11 @@ pub struct InMemoryBlockList {
 #[async_trait]
 impl BlockList for InMemoryBlockList {
     async fn block_user(&mut self, users_account_id: AccountId, blocked_account_id: AccountId) {
-        if let Some(vec) = self.block_list.lock().await.get_mut(&users_account_id) {
+        let mut block_list = self.block_list.lock().await;
+        if let Some(vec) = block_list.get_mut(&users_account_id) {
             vec.push(blocked_account_id);
         } else {
-            self.block_list
-                .lock()
-                .await
-                .insert(users_account_id, vec![blocked_account_id]);
+            block_list.insert(users_account_id, vec![blocked_account_id]);
         }
     }
 
@@ -33,5 +31,61 @@ impl BlockList for InMemoryBlockList {
             return list.contains(blocked_account_id);
         }
         false
+    }
+}
+
+mod test {
+    use sam_common::AccountId;
+
+    use crate::managers::{in_mem::InMemoryBlockList, traits::BlockList};
+
+    #[tokio::test]
+    async fn can_find_blocked_user() {
+        let mut block_list = InMemoryBlockList::default();
+
+        let users = vec![AccountId::generate(), AccountId::generate()];
+        let blocked_users = vec![
+            AccountId::generate(),
+            AccountId::generate(),
+            AccountId::generate(),
+        ];
+
+        for user in users.clone() {
+            for blocked_user in blocked_users.clone() {
+                block_list.block_user(user, blocked_user).await;
+            }
+        }
+
+        for user in users {
+            for blocked_user in blocked_users.clone() {
+                assert!(
+                    block_list
+                        .check_for_blocked_user(&user, &blocked_user)
+                        .await
+                )
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn cannot_find_not_blocked_user() {
+        let block_list = InMemoryBlockList::default();
+
+        let users = vec![AccountId::generate(), AccountId::generate()];
+        let not_blocked_users = vec![
+            AccountId::generate(),
+            AccountId::generate(),
+            AccountId::generate(),
+        ];
+
+        for user in users {
+            for blocked_user in not_blocked_users.clone() {
+                assert!(
+                    !block_list
+                        .check_for_blocked_user(&user, &blocked_user)
+                        .await
+                )
+            }
+        }
     }
 }
--- proxy/src/managers/in_mem/request.rs
+++ proxy/src/managers/in_mem/request.rs
@@ -14,14 +14,57 @@ pub struct InMemoryKeyRequestManager {
 #[async_trait]
 impl KeyRequestManager for InMemoryKeyRequestManager {
     async fn store_receiver(&mut self, sender: AccountId, receiver: AccountId) {
-        if let Some(vec) = self.requests.lock().await.get_mut(&sender) {
+        let mut requests = self.requests.lock().await;
+        if let Some(vec) = requests.get_mut(&sender) {
             vec.push(receiver);
         } else {
-            self.requests.lock().await.insert(sender, vec![receiver]);
+            requests.insert(sender, vec![receiver]);
         }
     }
 
     async fn get_receivers(&mut self, account_id: AccountId) -> Option<Vec<AccountId>> {
         self.requests.lock().await.remove(&account_id)
+    }
+}
+
+mod test {
+    use sam_common::AccountId;
+
+    use crate::managers::{in_mem::InMemoryKeyRequestManager, traits::KeyRequestManager};
+
+    #[tokio::test]
+    async fn can_get_stored_receivers() {
+        let mut request_manager = InMemoryKeyRequestManager::default();
+
+        let senders = vec![AccountId::generate(), AccountId::generate()];
+        let inserted_receivers = vec![AccountId::generate(); 32];
+
+        for sender in senders.clone() {
+            for receiver in inserted_receivers.clone() {
+                request_manager.store_receiver(sender, receiver).await;
+            }
+        }
+
+        for sender in senders {
+            let receivers = request_manager
+                .get_receivers(sender)
+                .await
+                .expect("Should contain receivers");
+            for inserted_receiver in inserted_receivers.clone() {
+                assert!(receivers.contains(&inserted_receiver))
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn can_get_none_receivers() {
+        let mut request_manager = InMemoryKeyRequestManager::default();
+
+        let senders = vec![AccountId::generate(), AccountId::generate()];
+
+        for sender in senders {
+            let receivers = request_manager.get_receivers(sender).await;
+            assert_eq!(receivers, None)
+        }
     }
 }
